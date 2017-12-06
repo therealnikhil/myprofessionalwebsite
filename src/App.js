70a20038caf2e920b5e29a004428bd0b0589226c
Nikhil Nand Kumar <nikhilfaips@gmail.com>
2017-12-07 03:18:04 +0530

    Smooth transitions for main sections, some more mockups

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { Button, Grid, Row, Col } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 //import faips_logo from './assets/faips-logo.jpg';
 //import purdue_logo from './assets/purdue-logo.png';
 import './App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: PropTypes.oneOf([0, 1, 2, 3, 4])
+    }
+  }
+  toggleSection(clickedSection) {
+    this.setState({
+      clicked: clickedSection
+    })
+  }
+  slideUpSectionDetais(clickedSection) {
+    var d = document.getElementById(`section-detail-${clickedSection}`);
+    d.className = "section-detail up";
+  }
+  slideDownSectionDetails(clickedSection) {
+    var d = document.getElementById(`section-detail-${clickedSection}`);
+    d.className = "section-detail down";
+  }
   render() {
     return (
       <div className="app">
@@ -15,17 +35,77 @@ export default class App extends Component {
         </header>
         <Grid className="fixed-container">
           <Row>
-            <Col id="about" className="main-section" md={3}>
-              <div></div>
+            <Col 
+              id="about" 
+              onMouseEnter={() => this.slideUpSectionDetais(1)} 
+              onMouseLeave={() => this.slideDownSectionDetails(1)} 
+              onClick={() => this.toggleSection(1)} 
+              className="main-section" 
+              md={3}
+            >
+              <div id={this.state.clicked === 1 ? "active-1" : "inactive-1"}>
+                <div id="section-detail-1" className="section-detail">
+                {
+                  this.state.clicked === 1 && 
+                  <div>
+                  </div>
+                }
+                </div>
+              </div>
             </Col>
-            <Col id="portfolio" className="main-section" md={3}>
-              <div></div>
+            <Col 
+              id="portfolio" 
+              onMouseEnter={() => this.slideUpSectionDetais(2)} 
+              onMouseLeave={() => this.slideDownSectionDetails(2)} 
+              onClick={() => this.toggleSection(2)} 
+              className="main-section" 
+              md={3}
+            >
+              <div id={this.state.clicked === 2 ? "active-2" : "inactive-2"}>
+                <div id="section-detail-2" className="section-detail">
+                {
+                  this.state.clicked === 2 && 
+                  <div>
+                  </div>
+                }
+                </div>
+              </div>
             </Col>
-            <Col id="interests" className="main-section" md={3}>
-              <div></div>
+            <Col 
+              id="interests" 
+              onMouseEnter={() => this.slideUpSectionDetais(3)} 
+              onMouseLeave={() => this.slideDownSectionDetails(3)} 
+              onClick={() => this.toggleSection(3)} 
+              className="main-section" 
+              md={3}
+            >
+              <div id={this.state.clicked === 3 ? "active-3" : "inactive-3"}>
+                <div id="section-detail-3" className="section-detail">
+                {
+                  this.state.clicked === 3 && 
+                  <div>
+                  </div>
+                }
+                </div>
+              </div>
             </Col>
-            <Col id="contact" className="main-section" md={3}>
-              <div></div>
+            <Col 
+              id="contact" 
+              onMouseEnter={() => this.slideUpSectionDetais(4)} 
+              onMouseLeave={() => this.slideDownSectionDetails(4)} 
+              onClick={() => this.toggleSection(4)} 
+              className="main-section" 
+              md={3}
+            >
+              <div id={this.state.clicked === 4 ? "active-4" : "inactive-4"}>
+                <div id="section-detail-4" className="section-detail">
+                {
+                  this.state.clicked === 4 && 
+                  <div>
+                  </div>
+                }
+                </div>
+              </div>
             </Col>
           </Row>
         </Grid>
